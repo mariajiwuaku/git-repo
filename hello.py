a99b3cbcd93c1aeaab80a0726fedba5cbd590e97
mariajiwuaku <mariagorettiduruigbo@gmail.com>
2023-06-18 19:59:17 +0100

    task done

---
+++ hello.py
@@ -0,0 +1,70 @@
+#Task Two:
+first_name = "Maria-goretti"
+middle_name = "Onyekachi"
+last_name = "Jiwuaku"
+print("My name is {} {} {}".format(first_name, middle_name, last_name))
+
+#Task Three:
+
+#String data type 
+name = "Maria"
+type_name = type(name)
+print(f"the name is {name}, and the data type is {type_name}")
+
+#Int data type
+amount = 100000
+type_amount = type(amount)
+print(f"the value of amount is {amount}, and the data type is {type_amount}")
+
+#float data type
+height = 1.65
+type_height = type(height)
+print(f"the value of amount is {height}, and the data type is {type_height}")
+
+#Bool data type
+is_real = True
+type_is_real = type(is_real)
+print(f"The value of is_real is {is_real}, data type is {type_is_real}")
+
+#List data type
+money = ["dollar", "euro", "pounds", "naira"]
+type_money = type(money)
+print(f"the values of money is {money}, and data type is {type_money}")
+
+#Tuple data type
+boy = ("obi", "John", "Uriel")
+type_boy = type(boy)
+print(f"the values of money is {boy}, and data type is {type_boy}")
+
+#Set data types
+numbers = {2, 4, 6, 8}
+type_numbers = type(numbers)
+print(f"the values of money is {numbers}, and data type is {type_numbers}")
+
+#Complex data type
+y = 2j
+type_y = type(y)
+print(f"the values of money is {y}, and data type is {type_y}")
+#None data type
+data = None
+type_data = type(data)
+print(f"the values of money is {data}, and data type is {type_data}")
+#Frozen set data type
+fruits = ({"apple", "orange", "guava"})
+type_fruits = type(fruits)
+print(f"the values of money is {fruits}, and data type is {type_fruits}")
+
+#Range data type
+exams = range(200)
+type_exams = type(exams)
+print(f"the values of money is {exams}, and data type is {type_exams}")
+
+#Bytes data type
+word = b'Great'
+type_word = type(word)
+print(f"the values of money is {word}, and data type is {type_word}")
+
+#Dict data type
+person = {"name": "Maria", "age":28, "city": "Abuja"}
+type_person =type(person)
+print(f"the values of money is {person}, and data type is {type_person}")
